96f19ceeb4b13cca3c844df66be4e94d387510f4
Indra Purnomo <indra.purnomo@mail.ugm.ac.id>
2017-05-25 19:50:03 +0700

    Update install.sh

--- Batch001/indrapurnomo/Peer-Kelas-DevOps-Binar/Peer-Install-Vagrant-Config/install.sh
+++ Batch001/indrapurnomo/Peer-Kelas-DevOps-Binar/Peer-Install-Vagrant-Config/install.sh
@@ -10,10 +10,10 @@ echo "Nginx Config"
  sudo cp -r /vagrant/kirana.jpg /var/www/html
  sudo service nginx restart
 echo "Instalasi mariaDB"
- apt-get install mariadb-server -y 
  apt-get install debconf-utils -y
  debconf-set-selections <<< "mariadb-server mysql-server/root_password password 1234"    
  debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password 1234"
+ apt-get install mariadb-server -y 
 echo "Buat User dan Hak Akses"
  mysql -u root -p1234 -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'ina' WITH GRANT OPTION;"
  mysql -u root -p1234 -e "CREATE USER 'nurina'@'localhost' IDENTIFIED BY 'ina';GRANT ALL PRIVILEGES ON * . * TO  'nurina'@'localhost';"
