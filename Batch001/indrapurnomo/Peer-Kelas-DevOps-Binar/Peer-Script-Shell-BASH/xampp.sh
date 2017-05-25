669683a94cd9e5d9aa46605ff106c63fb3e4ff5f
indra <indra.pazz@gmail.com>
2017-05-25 19:24:36 +0700

    update peer dan kuis

---
+++ Batch001/indrapurnomo/Peer-Kelas-DevOps-Binar/Peer-Script-Shell-BASH/xampp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Download XAMPP
+wget https://www.apachefriends.org/xampp-files/5.6.30/xampp-linux-x64-5.6.30-1-installer.run
+# Ubah Permissions
+sudo chmod 755 xampp-linux-x64-5.6.30-1-installer.run
+# Menjalankan XAMPP
+sudo ./xampp-linux-x64-5.6.30-1-installer.run
+
